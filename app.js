6ceafe830e3c23b0b544d0eb082859216012d97c
Praveen Gadikoppula <praveen.g@darwinbox.io>
2024-10-30 23:58:43 +0530

    docker file changes

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
+
 const moviesRoutes = require('./routes/movies');
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+app.use(cors({
+    origin: 'http://localhost:3000', // Adjust for your front-end URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 // Routes
 app.use('/api/movies', moviesRoutes);
